69dfd3b9ec7bb6d306cc97667f109870f7444539
RONALDBUZAGLO <senseironald@gmail.com>
2021-03-12 14:06:39 -0400

    Configurações do knex

--- database.js
+++ database.js
@@ -1,9 +1,11 @@
 const knex = require('knex')({
-    client: 'mysql',
+    client: 'mysql2',
     connection: {
       host : '127.0.0.1',
-      user : 'your_database_user',
-      password : 'your_database_password',
-      database : 'myapp_test'
+      user : 'root',
+      password : '123456',
+      database : 'knexjs'
     }
   });
+
+  module.exports = knex;
